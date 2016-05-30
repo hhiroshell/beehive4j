c3a5226911b9f097798a77de66767347dff384a1
charlier-shoe <charlier.shoe@gmail.com>
2016-05-30 23:36:12 +0900

    Add create invitation feature.

--- src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/model/MeetingParticipantUpdater.java
+++ src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/model/MeetingParticipantUpdater.java
@@ -1,1 +1,36 @@
 package jp.gr.java_conf.hhayakawa_jp.beehive_client.model;
+
+public final class MeetingParticipantUpdater {
+
+    private final String address;
+    private final String name;
+    private final MeetingParticipantUpdaterOperation operation;
+    private final BeeId participant;
+
+    public MeetingParticipantUpdater(String address, String name, MeetingParticipantUpdaterOperation operation,
+            BeeId participant) {
+        super();
+        this.address = address;
+        this.name = name;
+        this.operation = operation;
+        this.participant = participant;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MeetingParticipantUpdaterOperation getOperation() {
+        return operation;
+    }
+
+    public BeeId getParticipant() {
+        return participant;
+    }
+
+}
+
--- src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/model/MeetingUpdater.java
+++ src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/model/MeetingUpdater.java
@@ -1,5 +1,7 @@
 package jp.gr.java_conf.hhayakawa_jp.beehive_client.model;
 
+import java.util.List;
+
 import jp.gr.java_conf.hhayakawa_jp.beehive_client.BeehiveApiPayload;
 
 public class MeetingUpdater implements BeehiveApiPayload {
@@ -14,16 +16,23 @@ public class MeetingUpdater implements BeehiveApiPayload {
 //    private final CollabPropertiesUpdater inviteePropertiesUpdater;
     private final Transparency inviteeTransparency;
     private final String locationName;
-    // not yet support.
-//    private final List<MeetingParticipantUpdater> participantUpdaters;
+    private final List<MeetingParticipantUpdater> participantUpdaters;
     private final OccurrenceStatus status;
     private final String textDescription;
     private final String xhtmlFragmentDescription;
 
-    public MeetingUpdater(String start, String end, boolean includeOnlineConference,
-            OccurrenceParticipantStatus inviteeParticipantStatus, TimedTrigger inviteePrimaryClientReminderTrigger,
-            Priority inviteePriority, Transparency inviteeTransparency, String locationName, OccurrenceStatus status,
-            String textDescription, String xhtmlFragmentDescription) {
+    public MeetingUpdater(
+            String start, String end,
+            boolean includeOnlineConference,
+            OccurrenceParticipantStatus inviteeParticipantStatus,
+            TimedTrigger inviteePrimaryClientReminderTrigger,
+            Priority inviteePriority,
+            Transparency inviteeTransparency,
+            String locationName,
+            List<MeetingParticipantUpdater> participantUpdaters,
+            OccurrenceStatus status,
+            String textDescription,
+            String xhtmlFragmentDescription) {
         super();
         this.start = start;
         this.end = end;
@@ -33,6 +42,7 @@ public class MeetingUpdater implements BeehiveApiPayload {
         this.inviteePriority = inviteePriority;
         this.inviteeTransparency = inviteeTransparency;
         this.locationName = locationName;
+        this.participantUpdaters = participantUpdaters;
         this.status = status;
         this.textDescription = textDescription;
         this.xhtmlFragmentDescription = xhtmlFragmentDescription;
@@ -62,6 +72,7 @@ public class MeetingUpdater implements BeehiveApiPayload {
         return inviteePriority;
     }
 
+    // not yet support.
 //    public CollabPropertiesUpdater getInviteePropertiesUpdater() {
 //        return inviteePropertiesUpdater;
 //    }
@@ -74,9 +85,9 @@ public class MeetingUpdater implements BeehiveApiPayload {
         return locationName;
     }
 
-//    public List<ParticipantUpdater> getParticipantUpdaters() {
-//        return participantUpdaters;
-//    }
+    public List<MeetingParticipantUpdater> getParticipantUpdaters() {
+        return participantUpdaters;
+    }
 
     public OccurrenceStatus getStatus() {
         return status;
