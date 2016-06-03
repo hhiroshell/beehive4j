fcc27c893bbaf18a9e7681f09643ff127b163764
charlier-shoe <charlier.shoe@gmail.com>
2016-06-03 22:36:20 +0900

    Fix visibility.

--- src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/InvtCreateInvoker.java
+++ src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/InvtCreateInvoker.java
@@ -2,7 +2,7 @@ package jp.gr.java_conf.hhayakawa_jp.beehive_client;
 
 import org.springframework.http.HttpMethod;
 
-public class InvtCreateInvoker extends BeehiveInvoker {
+class InvtCreateInvoker extends BeehiveInvoker {
 
     private static final String PATH = "invt";
 
