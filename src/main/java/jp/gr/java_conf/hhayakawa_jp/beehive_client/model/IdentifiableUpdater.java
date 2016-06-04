8f2679e9ee7ddb6c42c12d32f51a9bcc607df122
charlier-shoe <charlier.shoe@gmail.com>
2016-06-04 23:43:34 +0900

    "beeType" should be final.

--- src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/model/IdentifiableUpdater.java
+++ src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/model/IdentifiableUpdater.java
@@ -7,7 +7,7 @@ import jp.gr.java_conf.hhayakawa_jp.beehive_client.BeehiveApiPayload;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class IdentifiableUpdater implements BeehiveApiPayload {
 
-    private String beeType = "identifiableUpdater";
+    private final String beeType = "identifiableUpdater";
 
     public String getBeeType() {
         return beeType;
