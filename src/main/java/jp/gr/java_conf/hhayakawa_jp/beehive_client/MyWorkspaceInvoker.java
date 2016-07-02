6ac78b2b0c5769eac19b213a1bd192c4ac7db0fe
charlier-shoe <charlier.shoe@gmail.com>
2016-07-02 23:40:07 +0900

    Expand visibility of Invokers to use it from outside of it's package.

--- src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/MyWorkspaceInvoker.java
+++ src/main/java/jp/gr/java_conf/hhayakawa_jp/beehive_client/MyWorkspaceInvoker.java
@@ -3,7 +3,7 @@ package jp.gr.java_conf.hhayakawa_jp.beehive_client;
 import org.springframework.http.HttpMethod;
 
 @SuppressWarnings("rawtypes")
-final class MyWorkspaceInvoker extends BeehiveInvoker {
+public final class MyWorkspaceInvoker extends BeehiveInvoker {
 
     private static final String PATH = "my/workspace";
 
